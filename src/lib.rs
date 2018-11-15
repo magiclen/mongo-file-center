281dba076c39fd63b51b080b14047832d3f7c943
Magic Len <len@magiclen.org>
2018-11-15 20:50:10 +0800

    fix typos

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 /*!
 # File Center on MongoDB
 
-This crate aims to build a easy-to-use and no-redundant file storage based on MongoDB.
+This crate aims to build an easy-to-use and no-redundant file storage based on MongoDB.
 
 For perennial files, each of them is unique in the database, and can be retrieved many times without limitation.
 
 For temporary files, they are allowed to be duplicated, but each instance can be retrieved only one time in a minute after it is created.
 
-The file data can be store in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KB**.
+The file data can be stored in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KB**.
 
 Temporary files are suggested to store in a collection, otherwise you have to **clear the garbage** in GridFS.
 
