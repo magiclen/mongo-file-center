0b8824e3a2c1c5b2e3291bd435a31f1927e51d2e
Magic Len <len@magiclen.org>
2019-03-22 15:24:15 +0800

    modify the default file size threshold

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ For perennial files, each of them is unique in the database, and can be retrieve
 
 For temporary files, they are allowed to be duplicated, but each instance can be retrieved only one time in a minute after it is created.
 
-The file data can be stored in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KB**.
+The file data can be stored in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KiB**.
 
 Temporary files are suggested to store in a collection, otherwise you have to **clear the garbage** in GridFS.
 
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ For perennial files, each of them is unique in the database, and can be retrieve
 
 For temporary files, they are allowed to be duplicated, but each instance can be retrieved only one time in a minute after it is created.
 
-The file data can be stored in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KB**.
+The file data can be stored in a collection or GridFS. It depends on the size of data. If the size is bigger than the threshold, it stores in GridFS, or it stores in a collection. The max threshold is **16770KB**. The default threshold is **255KiB**.
 
 Temporary files are suggested to store in a collection, otherwise you have to **clear the garbage** in GridFS.
 
@@ -97,7 +97,7 @@ const DEFAULT_MIME_TYPE: Mime = mime::APPLICATION_OCTET_STREAM;
 const COLLECTION_FILES_NAME: &str = "file_center";
 const COLLECTION_SETTINGS_NAME: &str = "file_center_settings";
 const MAX_FILE_SIZE_THRESHOLD: i32 = 16770000;
-const DEFAULT_FILE_SIZE_THRESHOLD: i32 = 255000;
+const DEFAULT_FILE_SIZE_THRESHOLD: i32 = 261120;
 
 const SETTING_FILE_SIZE_THRESHOLD: &str = "file_size_threshold";
 const SETTING_CREATE_TIME: &str = "create_time";
